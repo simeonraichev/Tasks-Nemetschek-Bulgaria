a0be018a832f1e23ae3b30c29095475738fa88b3
Simeon Raychev <simeon0022@gmail.com>
2023-01-28 00:11:05 +0200

    LinkedList Stack implemented

--- Stack implementation/Stack implementation/LinkedListStack.cs
+++ Stack implementation/Stack implementation/LinkedListStack.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Stack_implementation
 {
-    internal class LinkedListStack
+    class LinkedListStack<T> : IStack<T>
     {
+        private LinkedList<T> list;
+        public LinkedListStack()
+        {
+            list = new LinkedList<T>();
+        }
+        public void Push(T item)
+        {
+            list.AddFirst(item);
+        }
+        public T Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+            T item = list.First.Value;
+            list.RemoveFirst();
+            return item;
+        }
+        public T Peek()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+            return list.First.Value;
+        }
+        public int Count { get { return list.Count; } }
+        public bool IsEmpty { get { return list.Count == 0; } }
     }
 }
